956ba61480823adc2e0974c32966eadbfcd9a443
Quotoms <azo.cw@yandex.ru>
2022-04-19 12:45:36 +0300

    Получение IP

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Обработка главной страницы
+
+    :return:
+    """
+    # Можно использовать request.remote_addr или request.environ['REMOTE_ADDR']
+    ipaddr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    return f'<p>IP: {ipaddr}</p>'
+
 
 if __name__ == '__main__':
-    pass
+    app.run(debug=True)
